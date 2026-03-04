8a60939e31909bd38be3d8cb2df35eaf4a6abf2e
devth3us <matheus.silvaa3454@gmail.com>
2026-03-04 00:23:01 -0300

    Correcao na api de busca o usuarios

--- app.py
+++ app.py
@@ -412,4 +412,5 @@ def api_buscar_usuario():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
